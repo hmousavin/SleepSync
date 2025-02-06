2ee5139eaee06c04f77c06b156000c6c73ffce96
hmousavin <hamid.mousavinasab@gmail.com>
2025-02-06 09:45:39 +0330

    the layout applied again

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,14 +8,17 @@ import SignInPage from './pages/sign-in'
 import SignUpPage from './pages/sign-up'
 import NotFound from './pages/not-found'
 import './input.css'
+import RootLayout from './pages/layout'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={<SignInPage/>} />
-        <Route path="/sign-up" element={<SignUpPage/>} />
+        <Route element={<RootLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignInPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
--- client/src/pages/layout.tsx
+++ client/src/pages/layout.tsx
@@ -1,15 +1,15 @@
-import React from "react";
+import { Outlet } from "react-router-dom";  // Import Outlet
 import { ThemeProvider } from "../providers/theme-provider";
 import { ThemeToggle } from   "../components/theme-toggle";
 import { MobileFrame } from "../components/mobile-frame";
 import { BottomNav } from "../components/layout/bottom-nav";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout() {
   return (
-    <div className="font-sans"> {/* Apply font directly */}
+    <div className="font-sans">
       <ThemeProvider>
         <MobileFrame>
-          {children}
+          <Outlet />
           <BottomNav />
         </MobileFrame>
         <ThemeToggle />
