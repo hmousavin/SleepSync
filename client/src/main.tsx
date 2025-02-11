0cb201aeac8a075d9475576965a29364a4ccdb9f
hmousavin <hamid.mousavinasab@gmail.com>
2025-02-11 17:36:03 +0330

    add more routes to being handled

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,6 +6,11 @@ import SignInPage from './pages/sign-in'
 import SignUpPage from './pages/sign-up'
 import NotFound from './pages/not-found'
 import RootLayout from './pages/layout'
+import JournalPage from './pages/journal'
+import ReportPage from './pages/report'
+import SoundsPage from './pages/sounds'
+import AccountPage from './pages/account'
+import SleepPage from './pages/sleep'
 import './input.css'
 
 createRoot(document.getElementById('root')!).render(
@@ -13,10 +18,16 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <Routes>
         <Route element={<RootLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/sign-in" element={<SignInPage />} />
-          <Route path="/sign-up" element={<SignUpPage />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="/"        element={<Home/>} />
+          <Route path="/sign-in" element={<SignInPage/>} />
+          <Route path="/sign-up" element={<SignUpPage/>} />
+          <Route path="/sounds"  element={<SoundsPage/>} />
+          <Route path="/journal" element={<JournalPage/>} />
+          <Route path="/report"  element={<ReportPage/>} />
+          <Route path="/account" element={<AccountPage/>} />
+          <Route path="/sleep" element={<SleepPage/>} />
+          
+          <Route path="*"        element={<NotFound/>} />
         </Route>
       </Routes>
     </BrowserRouter>
