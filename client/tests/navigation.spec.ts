e8a0a4f0fe42ad52488638c5bf8774708377f645
hmousavin <hamid.mousavinasab@gmail.com>
2025-02-17 07:08:54 +0330

    onboarding and authentication test cases

--- client/tests/navigation.spec.ts
+++ client/tests/navigation.spec.ts
@@ -1,17 +1,38 @@
 import { test, expect } from "@playwright/test";
 
+const URL = "localhost";
+const PORT = "5173";
+
+const localEndpoint = `http://${URL}:${PORT}`;
+
 test.describe("SleepSync Navigation Tests", () => {
-  
-    test.fixme("navigates to home if an active login found", async ({ page }) => {
+    
+    test("navigates to onboarding if an active not found", async ({ page }) => {
+        await page.goto(localEndpoint);
+
+        await expect(page).toHaveURL("/onboarding")
+        await expect(page.locator("h1")).toHaveText("Let's Get Started!");
+    });
+    
+    test("navigates to home if an active login found", async ({ page }) => {
+        await page.goto(localEndpoint);
+
+        await expect(page).toHaveURL("/")
+        await expect(page.locator("h1")).toHaveText("Welcome back, User");
     });
 
-    test.fixme("navigates to getting-started if an active login didn't found", async ({ page }) => {
+    test("navigates to to sign-up by selecting that", async ({ page }) => {
+        await page.goto(`${localEndpoint}/sign-up`);
+
+        await expect(page).toHaveURL("/sign-up")
+        await expect(page.locator("h1")).toHaveText("Create an account");
     });
 
-    test.fixme("navigates to to sign-up by selecting that", async ({ page }) => {
-    });
+    test("navigates to to sign-in by selecting that", async ({ page }) => {
+        await page.goto(`${localEndpoint}/sign-in`);
 
-    test.fixme("navigates to to sign-in by selecting that", async ({ page }) => {
+        await expect(page).toHaveURL("/sign-in")
+        await expect(page.locator("h1")).toHaveText("Welcome back");
     });
 
     test.fixme("navigates to check-up after sign-up", async ({ page }) => {
