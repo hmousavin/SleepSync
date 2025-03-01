501c36ede05d05fdaf18e3c1b2adfcca1c9cd400
hmousavin <hamid.mousavinasab@gmail.com>
2025-03-01 22:03:19 +0330

    minor style tweaks

--- client/src/components/mobile-frame.tsx
+++ client/src/components/mobile-frame.tsx
@@ -2,7 +2,7 @@ import type React from "react"
 
 export function MobileFrame({ children }: { children: React.ReactNode }) {
   return (
-    <div className="mx-auto h-screen max-w-md overflow-hidden bg-blue-100 px-4 py-8 shadow-xl sm:rounded-xl sm:px-6 sm:py-12 md:my-8">
+    <div className="mx-auto min-h-screen max-w-md overflow-hidden bg-blue-100 px-4 py-8 shadow-xl sm:rounded-xl sm:px-6 sm:py-12 md:my-8">
       {children}
     </div>
   )
